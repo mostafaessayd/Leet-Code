02f6d184a7258dac477d56960fb77aa634af1b50
mohamedessayd <ballm3957@gmail.com>
2024-06-15 06:49:25 +0100

    Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * created by : cidi mohamed mostepha
+ * link : https://leetcode.com/problems/binary-tree-inorder-traversal
+ * Difficulty : easy
+ */
+
+#include<bits/stdc++.h>
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        vector<int> left = inorderTraversal(root->left);
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.push_back(root->val);
+        vector<int> right = inorderTraversal(root->right);
+        ans.insert(ans.end(), right.begin(), right.end());
+        return ans;
+    }
+};
