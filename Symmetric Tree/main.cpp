e5dd8985877073ebcaacaac5199a01313608aed4
mohamedessayd <ballm3957@gmail.com>
2024-06-17 16:25:32 +0100

    Symmetric Tree

---
+++ Symmetric Tree/main.cpp
@@ -0,0 +1,63 @@
+/**
+ * created by : cidi mohamed mostepha
+ * link : https://leetcode.com/problems/symmetric-tree
+ * Difficulty : easy
+ */
+
+class Solution
+{
+public:
+    vector<int> a;
+    vector<int> b;
+
+    void rempA(TreeNode *root)
+    {
+        if (root)
+        {
+            a.push_back(root->val);
+            rempA(root->left);
+            rempA(root->right);
+        }
+        else
+        {
+            a.push_back(-1);
+        }
+    }
+
+    void rempB(TreeNode *root)
+    {
+        if (root)
+        {
+            b.push_back(root->val);
+            rempB(root->right);
+            rempB(root->left);
+        }
+        else
+        {
+            b.push_back(-1);
+        }
+    }
+
+    bool isSymmetric(TreeNode *root)
+    {
+
+        rempA(root->left);
+        rempB(root->right);
+
+        if (a.size() != b.size())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            cout << a[i] << ' ' << b[i] << '\n';
+            if (a[i] != b[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
